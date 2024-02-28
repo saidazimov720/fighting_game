a39ec04301578d2f776fc9dd5c69b9e1103ed32d
saidazimov720 <saidazimovnurbek01gmail.com>
2024-02-28 16:05:43 +0500

    new fillstyle

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function animate() {
         window.requestAnimationFrame(animate);
+        c.fillStyle = 'black'
         c.fillRect(0, 0, canvas.width, canvas.height);
         player.update();
         enemy.update();
