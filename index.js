8649872668517db6c5a199b477cf59b35a63ac79
saidazimov720 <saidazimovnurbek01gmail.com>
2024-02-28 15:41:16 +0500

    format

--- index.js
+++ index.js
@@ -7,48 +7,48 @@ canvas.height = 576
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 class Sprite {
-    constructor({position, velocity}){
+    constructor({ position, velocity }) {
         this.position = position
         this.velocity = velocity
     }
-    draw(){
+    draw() {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, 50, 150)
     }
 
-    update(){
+    update() {
         this.draw()
-        this.position.y +=10 
+        this.position.y += 10
     }
 }
 
-const player = new Sprite ({
+const player = new Sprite({
     position: {
-    x:0,
-    y:0
-},
+        x: 0,
+        y: 0
+    },
     velocity: {
-    x:0,
-    y:0
+        x: 0,
+        y: 0
     }
 })
 
 
 
-const enemy = new Sprite ({
+const enemy = new Sprite({
     position: {
-    x:400,
-    y:100 
-},
+        x: 400,
+        y: 100
+    },
     velocity: {
-    x:0,
-    y:0
+        x: 0,
+        y: 0
     }
 })
 
 console.log(player);
 
-function animate(){
+function animate() {
     window.requestAnimationFrame(animate)
     player.update()
     enemy.update()
